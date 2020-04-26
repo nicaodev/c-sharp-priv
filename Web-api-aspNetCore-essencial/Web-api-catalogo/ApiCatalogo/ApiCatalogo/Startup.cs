b74a5c2f090547d5bc5cb71d9953456544353304
Nicaodev <alexandre.nicos@gmail.com>
2020-04-26 20:24:07 -0300

    feature/1.0.4 - Criação do ILogger e salvando em .txt.

--- Web-api-aspNetCore-essencial/Web-api-catalogo/ApiCatalogo/ApiCatalogo/Startup.cs
+++ Web-api-aspNetCore-essencial/Web-api-catalogo/ApiCatalogo/ApiCatalogo/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ApiCatalogo.Context;
 using ApiCatalogo.Extensions;
+using ApiCatalogo.Logging;
 using ApiCatalogo.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -42,12 +43,18 @@ namespace ApiCatalogo
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. The Middleware
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            loggerFactory.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+            {
+                LogLevel = LogLevel.Information
+            }));
+
             //add middleware de tratamento de erros.
             app.ConfigureExceptionHandler();
 
