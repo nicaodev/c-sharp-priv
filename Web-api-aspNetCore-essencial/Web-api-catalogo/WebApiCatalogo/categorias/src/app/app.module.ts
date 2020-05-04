c51f24684870b1b5df4146fbb993d405ecc0ede2
Nicaodev <alexandre.nicos@gmail.com>
2020-05-03 21:37:17 -0300

    feature/2.1.2 - Criacao de menu c Material Design.

--- Web-api-aspNetCore-essencial/Web-api-catalogo/WebApiCatalogo/categorias/src/app/app.module.ts
+++ Web-api-aspNetCore-essencial/Web-api-catalogo/WebApiCatalogo/categorias/src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriasComponent } from './categorias/categorias.component';
 import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
 import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
 import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule } from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 
@@ -17,12 +34,28 @@ import { LogoutComponent } from './logout/logout.component';
     CategoriaDetalheComponent,
     CategoriaNovaComponent,
     CategoriaEditarComponent,
+    MenuComponent,
     LoginComponent,
-    LogoutComponent
+    LogoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
